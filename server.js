a29d34608cd99436bd87877034f87f640f0b1a0f
DevTabibur <tobiburrohman2@gmail.com>
2022-05-19 08:11:27 +0600

    all done

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -53,19 +54,26 @@ async function run() {
         res.send(result)
     })
 
-    // get add task data by post
-    app.post("/add-task", async (req, res) => {
+    // post add task data by post
+    app.post("/tasks", async (req, res) => {
       const newTask = req.body;
       const result = await taskCollection.insertOne(newTask);
       res.send(result);
     });
 
+    app.get("/tasks", async (req, res) => {
+        const query = {};
+        const cursor = taskCollection.find(query);
+        const tasks = await cursor.toArray();
+        res.send(tasks);
+    });
+
     // delete task
     app.delete("/tasks/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await taskCollection.deleteOne(query);
-      res.send(result);
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await taskCollection.deleteOne(query);
+        res.send(result);
     });
   } 
   finally {}
